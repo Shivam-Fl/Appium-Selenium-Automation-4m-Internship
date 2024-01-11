2f21a884a56e904b72f3d7b4135ed1807d95a906
Shivam Mishra <126186920+Shivam-Fl@users.noreply.github.com>
2024-01-11 22:32:16 +0530

    Updated dropbox.py

--- dropbox.py
+++ dropbox.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 def DropboxLogin(email):
     service = Service(executable_path='chromedriver.exe')
     driver = webdriver.Chrome(service=service)
@@ -13,26 +12,26 @@ def DropboxLogin(email):
     try:
         wait = WebDriverWait(driver, 10)
 
-        # Find the email input field
+        # Find the email input field using Xpath
         email_input = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@type="email"]')))
         email_input.clear()
         email_input.send_keys(email)
 
-        # Click on "Continue" span
+        # Click on "Continue" span using Xpath
         continue_span = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Continue"]')))
         continue_span.click()
 
         try:
-            # Check for the span with text "Welcome back"
+            # Check for the span with text "Welcome back" using Xpath
             welcome_back_span = wait.until(
                 EC.visibility_of_element_located((By.XPATH, '//span[text()="Welcome back"]')))
-
+            
             # If the welcome back span is found, user is registered
             if welcome_back_span:
                 return {"User Registered": True}
 
         except:
-            # Check for the span with text "Sign up for free"
+            # Check for the span with text "Sign up for free" using Xpath
             sign_up_span = wait.until(
                 EC.visibility_of_element_located((By.XPATH, '//span[text()="Sign up for free"]')))
 
@@ -46,7 +45,6 @@ def DropboxLogin(email):
 
     finally:
         driver.quit()
-
 
 if __name__ == "__main__":
     email = input("Enter your Dropbox email: ")
